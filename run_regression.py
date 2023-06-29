6b8ddb265ce429f1aa9d13b0e6298f92e7a566a9
YarinBS <yarin10121@gmail.com>
2023-06-30 01:11:22 +0300

    done (needs testing)

--- run_regression.py
+++ run_regression.py
@@ -1,13 +1,76 @@
 import pickle
 import sys
 import pandas as pd
+from math import sqrt
 
 
 def run_regression(model_path, sample_path):
     model = pickle.load(open(model_path, 'rb'))
-    sample = pd.read_csv(sample_path)
+    sample = pd.read_csv(sample_path, skiprows=5)
+    sample.name = sample_path[sample_path.rfind('/'):]
+    steps_count = int(list(pd.read_csv(sample_path, skiprows=lambda x: x != 3).columns)[1])
+    sample = processing(sample, steps=steps_count)
     prediction = model.predict(sample)
     return prediction
+
+
+def processing(df, steps):
+    df['N'] = df.apply(lambda x: sqrt(x['ACC X'] ** 2 + x['ACC Y'] ** 2 + x['ACC Z'] ** 2), axis=1)
+    df['steps_count'] = steps
+
+    stats_df = df.describe().T
+
+    number_of_timestamps = stats_df['count'][0]
+    recording_duration = stats_df['max'][0] - stats_df['min'][0]
+    steps_count = stats_df['mean'][4]
+
+    accx_mean = stats_df['mean'][1]
+    accx_std = stats_df['std'][1]
+    accx_min = stats_df['min'][1]
+    accx_max = stats_df['max'][1]
+    accx_median = stats_df['50%'][1]
+    accx_interval = accx_max - accx_min
+
+    accy_mean = stats_df['mean'][2]
+    accy_std = stats_df['std'][2]
+    accy_min = stats_df['min'][2]
+    accy_max = stats_df['max'][2]
+    accy_median = stats_df['50%'][2]
+    accy_interval = accy_max - accy_min
+
+    accz_mean = stats_df['mean'][3]
+    accz_std = stats_df['std'][3]
+    accz_min = stats_df['min'][3]
+    accz_max = stats_df['max'][3]
+    accz_median = stats_df['50%'][3]
+    accz_interval = accz_max - accz_min
+
+    N_mean = stats_df['mean'][5]
+    N_std = stats_df['std'][5]
+    N_min = stats_df['min'][5]
+    N_max = stats_df['max'][5]
+    N_median = stats_df['50%'][5]
+    N_interval = N_max - N_min
+
+    activity_type = 1 if 'walk' in df.name else 0
+
+    row = [number_of_timestamps, recording_duration, steps_count,
+           accx_mean, accx_std, accx_min, accx_max, accx_median, accx_interval,
+           accy_mean, accy_std, accy_min, accy_max, accy_median, accy_interval,
+           accz_mean, accz_std, accz_min, accz_max, accz_median, accz_interval,
+           N_mean, N_std, N_min, N_max, N_median, N_interval, activity_type]
+
+    stats_dataframe = pd.DataFrame(row, columns=['number_of_timestamps', 'recording_duration', 'steps_count',
+                                                 'accx_mean', 'accx_std', 'accx_min', 'accx_max', 'accx_median',
+                                                 'accx_interval',
+                                                 'accy_mean', 'accy_std', 'accy_min', 'accy_max', 'accy_median',
+                                                 'accy_interval',
+                                                 'accz_mean', 'accz_std', 'accz_min', 'accz_max', 'accz_median',
+                                                 'accz_interval',
+                                                 'N_mean', 'N_std', 'N_min', 'N_max', 'N_median', 'N_interval',
+                                                 'activity_type'])
+
+    return stats_dataframe
 
 
 def main(path):
